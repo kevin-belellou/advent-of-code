5a5f2947c066db00de6b275c1c5a715e00b64e4e
Kévin Belellou <kevin.belellou@gmail.com>
2024-11-22 12:26:26 +0100

    Added (deactivated) Day 8 of 2015

---
+++ src/main/java/com/belellou/kevin/advent/year2015/Day8.java
@@ -0,0 +1,94 @@
+package com.belellou.kevin.advent.year2015;
+
+import java.io.BufferedReader;
+import java.util.List;
+
+import com.belellou.kevin.advent.generic.AbstractDaySolver;
+import com.belellou.kevin.advent.generic.Day;
+import com.belellou.kevin.advent.generic.DisableTest;
+import com.belellou.kevin.advent.generic.Year;
+
+@SuppressWarnings("unused")
+@DisableTest
+public class Day8 extends AbstractDaySolver {
+
+    public Day8() {
+        super(Year.YEAR_2015, Day.DAY_8);
+    }
+
+    private static int countMemoryLength(String line) {
+        int memoryLength = -2;
+        boolean foundBackslash = false;
+        int hexadecimalCharCount = 0;
+
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+
+            if (c == '\\') {
+                foundBackslash = !foundBackslash;
+
+                if (foundBackslash) {
+                    continue;
+                }
+            } else if (foundBackslash && c == 'x') {
+                hexadecimalCharCount++;
+                memoryLength++;
+                foundBackslash = false;
+                continue;
+            }
+
+            if (hexadecimalCharCount == 3) {
+                hexadecimalCharCount = 0;
+            } else if (hexadecimalCharCount > 0) {
+                hexadecimalCharCount++;
+                continue;
+            }
+
+            memoryLength++;
+            foundBackslash = false;
+        }
+
+//        System.out.println("Line " + line + " code length is " + line.length());
+//        System.out.println("Line " + line + " memory length is " + memoryLength);
+
+        if (line.length() - memoryLength < 2) {
+            throw new IllegalStateException("wat ");
+        }
+        if (memoryLength < 0) {
+            throw new IllegalStateException("wut");
+        }
+        if (!line.startsWith("\"") || !line.endsWith("\"")) {
+            throw new IllegalStateException("wot");
+        }
+
+        return memoryLength;
+    }
+
+    @Override
+    protected int doSolveFirstStar(BufferedReader reader) {
+        List<String> lines = reader.lines().toList();
+
+        int totalCodeLength = lines.stream().map(String::length).reduce(Integer::sum).orElseThrow();
+        int totalMemoryLength = lines.stream().map(Day8::countMemoryLength).reduce(Integer::sum).orElseThrow();
+
+//        System.out.println("total code length is " + totalCodeLength);
+//        System.out.println("total memory length is " + totalMemoryLength);
+
+        return totalCodeLength - totalMemoryLength;
+    }
+
+    @Override
+    public int getFirstStarSolution() {
+        return 0;
+    }
+
+    @Override
+    protected int doSolveSecondStar(BufferedReader reader) {
+        return 0;
+    }
+
+    @Override
+    public int getSecondStarSolution() {
+        return 0;
+    }
+}
