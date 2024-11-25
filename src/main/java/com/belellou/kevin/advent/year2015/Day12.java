b87dc94d0aeed3b4d34e4285e0d3ae044429c89d
Kévin Belellou <kevin.belellou@gmail.com>
2024-11-25 15:14:34 +0100

    Added 2015 Day 12

---
+++ src/main/java/com/belellou/kevin/advent/year2015/Day12.java
@@ -0,0 +1,79 @@
+package com.belellou.kevin.advent.year2015;
+
+import java.io.BufferedReader;
+import java.util.Collection;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import org.jgrapht.util.ModifiableInteger;
+
+import com.belellou.kevin.advent.generic.AbstractDaySolver;
+
+@SuppressWarnings("unused")
+public class Day12 extends AbstractDaySolver<Integer> {
+
+    private static final String RED = "red";
+
+    public Day12() {
+        super(Day12.class);
+    }
+
+    private static void readMap(Map<?, ?> map, ModifiableInteger count, boolean stopForRed) {
+        if (stopForRed && map.containsValue(RED)) {
+            return;
+        }
+
+        readCollection(map.keySet(), count, stopForRed);
+        readCollection(map.values(), count, stopForRed);
+    }
+
+    private static void readCollection(Collection<?> collection, ModifiableInteger count, boolean stopForRed) {
+        for (Object o : collection) {
+            if (o instanceof Collection<?> objects) {
+                readCollection(objects, count, stopForRed);
+            } else if (o instanceof Map<?, ?> map) {
+                readMap(map, count, stopForRed);
+            } else {
+                try {
+                    count.setValue((int) (count.getValue() + Double.parseDouble(o.toString())));
+                } catch (NumberFormatException e) {
+                    // Do nothing
+                }
+            }
+        }
+    }
+
+    @Override
+    protected Integer doSolveFirstStar(BufferedReader reader) {
+        String line = reader.lines().findFirst().orElseThrow();
+
+        Map<?, ?> map = new Gson().fromJson(line, Map.class);
+        ModifiableInteger count = new ModifiableInteger(0);
+
+        readMap(map, count, false);
+
+        return count.getValue();
+    }
+
+    @Override
+    public Integer getFirstStarSolution() {
+        return 111_754;
+    }
+
+    @Override
+    protected Integer doSolveSecondStar(BufferedReader reader) {
+        String line = reader.lines().findFirst().orElseThrow();
+
+        Map<?, ?> map = new Gson().fromJson(line, Map.class);
+        ModifiableInteger count = new ModifiableInteger(0);
+
+        readMap(map, count, true);
+
+        return count.getValue();
+    }
+
+    @Override
+    public Integer getSecondStarSolution() {
+        return 65_402;
+    }
+}
