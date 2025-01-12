bc60c159ec9b82dbadc496c346022cd9b4ff99b3
Kévin Belellou <kevin.belellou@gmail.com>
2025-01-12 14:07:00 +0100

    Add package-info

---
+++ src/main/java/com/belellou/kevin/advent/year0/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Year 0 is a special package: it only contains the Day0 class, which is a template for all other days.
+ */
+package com.belellou.kevin.advent.year0;
---
+++ src/main/java/com/belellou/kevin/advent/year2016/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Contains the solutions for the year 2016.
+ */
+package com.belellou.kevin.advent.year2016;
