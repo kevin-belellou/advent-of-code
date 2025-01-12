9c0b4a3d184f82949a867c390c77ecf1631c8e54
Kévin Belellou <kevin.belellou@gmail.com>
2025-01-12 23:10:47 +0100

    Added 2016 Day 6

---
+++ src/main/java/com/belellou/kevin/advent/year2016/Day6.java
@@ -0,0 +1,76 @@
+package com.belellou.kevin.advent.year2016;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.belellou.kevin.advent.generic.AbstractDaySolver;
+
+@SuppressWarnings("unused")
+public class Day6 extends AbstractDaySolver<String> {
+
+    public Day6() {
+        super(Day6.class);
+    }
+
+    private static List<HashMap<Character, Integer>> extractFrequencies(BufferedReader reader) {
+        List<HashMap<Character, Integer>> frequencies = new ArrayList<>();
+
+        reader.lines().forEach(line -> {
+            for (int i = 0; i < line.length(); i++) {
+                char c = line.charAt(i);
+
+                if (frequencies.size() <= i) {
+                    frequencies.add(new HashMap<>());
+                }
+
+                frequencies.get(i).put(c, frequencies.get(i).getOrDefault(c, 0) + 1);
+            }
+        });
+
+        return frequencies;
+    }
+
+    @Override
+    protected String doSolveFirstStar(BufferedReader reader) {
+        List<HashMap<Character, Integer>> frequencies = extractFrequencies(reader);
+
+        return frequencies.stream()
+                          .map(frequency -> frequency.entrySet()
+                                                     .stream()
+                                                     .max(Comparator.comparingInt(Map.Entry::getValue))
+                                                     .orElseThrow())
+                          .map(Map.Entry::getKey)
+                          .map(String::valueOf)
+                          .reduce(String::concat)
+                          .orElseThrow();
+    }
+
+    @Override
+    public String getFirstStarSolution() {
+        return "agmwzecr";
+    }
+
+    @Override
+    protected String doSolveSecondStar(BufferedReader reader) {
+        List<HashMap<Character, Integer>> frequencies = extractFrequencies(reader);
+
+        return frequencies.stream()
+                          .map(frequency -> frequency.entrySet()
+                                                     .stream()
+                                                     .min(Comparator.comparingInt(Map.Entry::getValue))
+                                                     .orElseThrow())
+                          .map(Map.Entry::getKey)
+                          .map(String::valueOf)
+                          .reduce(String::concat)
+                          .orElseThrow();
+    }
+
+    @Override
+    public String getSecondStarSolution() {
+        return "owlaxqvq";
+    }
+}
